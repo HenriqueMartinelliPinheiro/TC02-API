a0abd3ecb9486e20744fcf7d70208cfb6249f7bb
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-08-03 15:13:41 -0300

    correcao teste login

--- src/__tests__/controllers/user/LoginUserController.test.ts
+++ src/__tests__/controllers/user/LoginUserController.test.ts
@@ -17,139 +17,146 @@ import { LoginUserController } from '../../../controllers/user/LoginUserControll
 vi.mock('../../../utils/validations/isValidEmail');
 vi.mock('../../../utils/validations/isValidPassword');
 vi.mock('../../../utils/validations/isValidRequest');
-vi.mock('../../../utils/generateUserErrorResponse', ()=>{
-    return {
-        generateUserErrorResponse: vi.fn(),
-    };
+vi.mock('../../../utils/generateUserErrorResponse', () => {
+	return {
+		generateUserErrorResponse: vi.fn(),
+	};
 });
 vi.mock('../../../loggers/Logger', () => {
-    return {
-        Logger: vi.fn().mockImplementation(() => {
-            return {
-                warn: vi.fn(),
-                error: vi.fn(),
-                info: vi.fn(),
-            };
-        }),
-    };
+	return {
+		Logger: vi.fn().mockImplementation(() => {
+			return {
+				warn: vi.fn(),
+				error: vi.fn(),
+				info: vi.fn(),
+			};
+		}),
+	};
 });
 
 describe('LoginUserController', () => {
-    let loginUserController: LoginUserController;
-    let req: Partial<Request>;
-    let res: Partial<Response>;
-    let loginUserService: LoginUserService;
-    let userRepository: IUserRepository;
-    let prismaClient: PrismaClient;
+	let loginUserController: LoginUserController;
+	let req: Partial<Request>;
+	let res: Partial<Response>;
+	let loginUserService: LoginUserService;
+	let userRepository: IUserRepository;
+	let prismaClient: PrismaClient;
 
-    beforeEach(() => {
-        // Mocks do repositório e logger
-        prismaClient = new PrismaClient();
-        userRepository = new UserRepository(prismaClient);
+	beforeEach(() => {
+		// Mocks do repositório e logger
+		prismaClient = new PrismaClient();
+		userRepository = new UserRepository(prismaClient);
 
-        loginUserService = new LoginUserService(userRepository);
-        loginUserService.execute = vi.fn(); // Mock do método execute
+		loginUserService = new LoginUserService(userRepository);
+		loginUserService.execute = vi.fn(); // Mock do método execute
 
-        loginUserController = new LoginUserController(loginUserService);
+		loginUserController = new LoginUserController(loginUserService);
 
-        req = {
-            body: {
-                userEmail: 'test@example.com',
-                userPassword: 'Password123',
-                requestUserEmail: 'test@example.com',
-            },
-        };
+		req = {
+			body: {
+				userEmail: 'test@example.com',
+				userPassword: 'Password123',
+				requestUserEmail: 'test@example.com',
+			},
+		};
 
-        res = {
-            status: vi.fn().mockReturnThis(),
-            json: vi.fn().mockReturnThis(),
-            setHeader: vi.fn(),
-        };
-    });
+		res = {
+			status: vi.fn().mockReturnThis(),
+			json: vi.fn().mockReturnThis(),
+			setHeader: vi.fn(),
+			cookie: vi.fn(),
+		};
+	});
 
-    it('should return 400 if request is invalid', async () => {
-        (isValidRequest as any).mockReturnValue(false);
+	it('should return 400 if request is invalid', async () => {
+		(isValidRequest as any).mockReturnValue(false);
 
-        await loginUserController.loginUser(req as Request, res as Response);
+		await loginUserController.loginUser(req as Request, res as Response);
 
-        expect(isValidRequest).toHaveBeenCalledWith(req.body, loginUserTypes);
-        expect(generateUserErrorResponse).toHaveBeenCalledWith(res, "Dados Inválidos", 400);
-    });
+		expect(isValidRequest).toHaveBeenCalledWith(req.body, loginUserTypes);
+		expect(generateUserErrorResponse).toHaveBeenCalledWith(res, 'Dados Inválidos', 400);
+	});
 
-    it('should return 400 if password is invalid', async () => {
-        (isValidRequest as any).mockReturnValue(true);
-        (isValidPassword as any).mockReturnValue(false);
+	it('should return 400 if password is invalid', async () => {
+		(isValidRequest as any).mockReturnValue(true);
+		(isValidPassword as any).mockReturnValue(false);
 
-        await loginUserController.loginUser(req as Request, res as Response);
+		await loginUserController.loginUser(req as Request, res as Response);
 
-        expect(isValidPassword).toHaveBeenCalledWith(req.body.userPassword);
-        expect(generateUserErrorResponse).toHaveBeenCalledWith(res, "Senha Inválida", 400);
-    });
+		expect(isValidPassword).toHaveBeenCalledWith(req.body.userPassword);
+		expect(generateUserErrorResponse).toHaveBeenCalledWith(res, 'Senha Inválida', 400);
+	});
 
-    it('should return 400 if email is invalid', async () => {
-        (isValidRequest as any).mockReturnValue(true);
-        (isValidPassword as any).mockReturnValue(true);
-        (isValidEmail as any).mockReturnValue(false);
+	it('should return 400 if email is invalid', async () => {
+		(isValidRequest as any).mockReturnValue(true);
+		(isValidPassword as any).mockReturnValue(true);
+		(isValidEmail as any).mockReturnValue(false);
 
-        await loginUserController.loginUser(req as Request, res as Response);
+		await loginUserController.loginUser(req as Request, res as Response);
 
-        expect(isValidEmail).toHaveBeenCalledWith(req.body.userEmail);
-        expect(generateUserErrorResponse).toHaveBeenCalledWith(res, "Email Inválido", 400);
-    });
+		expect(isValidEmail).toHaveBeenCalledWith(req.body.userEmail);
+		expect(generateUserErrorResponse).toHaveBeenCalledWith(res, 'Email Inválido', 400);
+	});
 
-    it('should return 401 if email or password is incorrect', async () => {
-        (isValidRequest as any).mockReturnValue(true);
-        (isValidPassword as any).mockReturnValue(true);
-        (isValidEmail as any).mockReturnValue(true);
+	it('should return 401 if email or password is incorrect', async () => {
+		(isValidRequest as any).mockReturnValue(true);
+		(isValidPassword as any).mockReturnValue(true);
+		(isValidEmail as any).mockReturnValue(true);
 
-        (loginUserService.execute as any).mockResolvedValue(undefined);
+		(loginUserService.execute as any).mockResolvedValue(undefined);
 
-        await loginUserController.loginUser(req as Request, res as Response);
+		await loginUserController.loginUser(req as Request, res as Response);
 
-        expect(loginUserService.execute).toHaveBeenCalledWith(expect.any(UserDomain));
-        expect(res.status).toHaveBeenCalledWith(401);
-        expect(res.json).toHaveBeenCalledWith({
-            user: undefined,
-            msg: 'Email ou senha incorretos',
-        });
-    });
-    
-    it('should return 201 if user is logged in successfully', async () => {
-        (isValidRequest as any).mockReturnValue(true);
-        (isValidPassword as any).mockReturnValue(true);
-        (isValidEmail as any).mockReturnValue(true);
+		expect(loginUserService.execute).toHaveBeenCalledWith(expect.any(UserDomain));
+		expect(res.status).toHaveBeenCalledWith(401);
+		expect(res.json).toHaveBeenCalledWith({
+			user: undefined,
+			msg: 'Email ou senha incorretos',
+		});
+	});
 
-        const user = new UserDomain({
-            userEmail: 'test@example.com',
-            userPassword: 'Password123',
-        });
-        user.getAccessToken = vi.fn().mockReturnValue('access-token');
-        user.getUserId = vi.fn().mockReturnValue(1);
+	it('should return 201 if user is logged in successfully', async () => {
+		(isValidRequest as any).mockReturnValue(true);
+		(isValidPassword as any).mockReturnValue(true);
+		(isValidEmail as any).mockReturnValue(true);
 
-        (loginUserService.execute as any).mockResolvedValue(user);
+		const user = new UserDomain({
+			userEmail: 'test@example.com',
+			userPassword: 'Password123',
+		});
+		user.getAccessToken = vi.fn().mockReturnValue('access-token');
+		user.getAccessTokenExpiration = vi.fn().mockReturnValue('expiration-date');
+		user.getUserId = vi.fn().mockReturnValue(1);
 
-        await loginUserController.loginUser(req as Request, res as Response);
+		(loginUserService.execute as any).mockResolvedValue(user);
 
-        expect(loginUserService.execute).toHaveBeenCalledWith(expect.any(UserDomain));
-        expect(res.setHeader).toHaveBeenCalledWith('x-access-token', 'access-token');
-        expect(res.status).toHaveBeenCalledWith(201);
-        expect(res.json).toHaveBeenCalledWith({
-            user,
-            msg: 'Usuário logado com sucesso',
-        });
-    });
+		await loginUserController.loginUser(req as Request, res as Response);
 
-    it('should return 500 if there is an internal server error', async () => {
-        (isValidRequest as any).mockReturnValue(true);
-        (isValidPassword as any).mockReturnValue(true);
-        (isValidEmail as any).mockReturnValue(true);
+		expect(loginUserService.execute).toHaveBeenCalledWith(expect.any(UserDomain));
+		expect(res.status).toHaveBeenCalledWith(201);
+		expect(res.json).toHaveBeenCalledWith({
+			user,
+			msg: 'Usuário logado com sucesso',
+			accessToken: user.getAccessToken(),
+			accessTokenExpiration: user.getAccessTokenExpiration(),
+		});
+	});
 
-        const error = new Error('Internal Server Error');
-        (loginUserService.execute as any).mockRejectedValue(error);
+	it('should return 500 if there is an internal server error', async () => {
+		(isValidRequest as any).mockReturnValue(true);
+		(isValidPassword as any).mockReturnValue(true);
+		(isValidEmail as any).mockReturnValue(true);
 
-        await loginUserController.loginUser(req as Request, res as Response);
+		const error = new Error('Internal Server Error');
+		(loginUserService.execute as any).mockRejectedValue(error);
 
-        expect(loginUserService.execute).toHaveBeenCalledWith(expect.any(UserDomain));
-        expect(generateUserErrorResponse).toHaveBeenCalledWith(res, "Erro interno do servidor", 500);
-    });
+		await loginUserController.loginUser(req as Request, res as Response);
+
+		expect(loginUserService.execute).toHaveBeenCalledWith(expect.any(UserDomain));
+		expect(generateUserErrorResponse).toHaveBeenCalledWith(
+			res,
+			'Erro interno do servidor',
+			500
+		);
+	});
 });
