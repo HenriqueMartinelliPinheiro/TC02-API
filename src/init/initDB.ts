ffefa22e90e7c570b029940505f7464cdc2355ea
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-08-12 20:43:22 -0300

    Retornando Roles no Login

--- src/init/initDB.ts
+++ src/init/initDB.ts
@@ -5,20 +5,25 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  try {
-    const roleAdmin = await prisma.role.create({
-      data: {
-        roleTitle: 'Admin',
-      },
-    });
+	try {
+		// const roleAdmin = await prisma.role.create({
+		// 	data: {
+		// 		roleTitle: 'Admin',
+		// 	},
+		// });
 
-  } catch (error) {
-    console.error('Erro ao inserir Role:', error);
-  } finally {
-    await prisma.$disconnect();
-  }
+		const roleOrganzador = await prisma.role.create({
+			data: {
+				roleTitle: 'Organizador',
+			},
+		});
+	} catch (error) {
+		console.error('Erro ao inserir Role:', error);
+	} finally {
+		await prisma.$disconnect();
+	}
 }
 
 main().catch((e) => {
-  throw e;
+	throw e;
 });
