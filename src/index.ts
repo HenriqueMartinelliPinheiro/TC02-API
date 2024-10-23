fc62ced960c1facd257dc6425f30977c28eb15fc
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-10-23 20:07:59 -0300

    correcao cors servidor ctri

--- src/index.ts
+++ src/index.ts
@@ -20,22 +20,13 @@ const app: Express = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',');
-
 app.use(
 	cors({
-		origin: (origin, callback) => {
-			if (!origin || allowedOrigins?.indexOf(origin) !== -1) {
-				callback(null, true);
-			} else {
-				callback(new Error('Not allowed by CORS'));
-			}
-		},
+		origin: 'https://cti.videira.ifc.edu.br',
 		allowedHeaders: ['Content-Type', 'Authorization'],
 		credentials: true,
 	})
 );
-
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
