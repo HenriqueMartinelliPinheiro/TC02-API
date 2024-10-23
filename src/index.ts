e761f4021300be8ce54675bb435e0ec4d7f9fdc1
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-10-23 20:18:26 -0300

    correcao cors servidor cti

--- src/index.ts
+++ src/index.ts
@@ -22,11 +22,17 @@ app.use(cookieParser());
 
 app.use(
 	cors({
-		origin: 'https://cti.videira.ifc.edu.br',
+		origin: [
+			'https://cti.videira.ifc.edu.br',
+			'http://localhost:5173',
+			'http://200.135.55.14:5173',
+			'http://172.19.0.1:5173',
+		],
 		allowedHeaders: ['Content-Type', 'Authorization'],
 		credentials: true,
 	})
 );
+
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
