2f6c717c72dea37837e25ebe6e463b178f673101
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-10-26 13:58:39 -0300

    teste obter token govbr

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,40 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
-import axios from 'axios';
+import { eventRouter } from './routes/eventRoutes';
+import { userRouter } from './routes/userRoutes';
 import { swaggerOptions } from './config/swaggerOptions';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import { courseRouter } from './routes/courseRoutes';
 import cookieParser from 'cookie-parser';
-import path from 'path';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { roleRouter } from './routes/roleRoutes';
+import { studentRouter } from './routes/studentRoutes';
+import { attendanceRouter } from './routes/students/attendanceRoutes';
+import { IssueReportService } from './services/report/IssueReportsService';
+import { ScheduleProcessor } from './utils/ScheduleProcess';
+import reportsRouter from './routes/reportsRoutes';
+import { eventStudentRouter } from './routes/students/eventRoutes';
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+// const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',');
+// app.use(
+// 	cors({
+// 		origin: (origin, callback) => {
+// 			if (!origin || allowedOrigins?.indexOf(origin) !== -1) {
+// 				callback(null, true);
+// 			} else {
+// 				callback(new Error('Not allowed by CORS'));
+// 			}
+// 		},
+// 		allowedHeaders: ['Content-Type', 'Authorization'],
+// 		credentials: true,
+// 	})
+// );
+
 app.use(
 	cors({
 		origin: ['http://200.135.55.14:5173', 'http://localhost:5173'],
@@ -23,44 +44,17 @@ app.use(
 );
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
 app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Endpoint to handle govbr token exchange
-app.post('/api/govbr/token', async (req: Request, res: Response) => {
-	try {
-		const { code } = req.body;
-		if (!code) {
-			return res.status(400).json({ message: 'Code is required' });
-		}
-
-		const config = {
-			URL_PROVIDER: process.env.GOVBR_URL_PROVIDER!,
-			URL_SERVICE: process.env.GOVBR_URL_SERVICE!,
-			REDIRECT_URI: process.env.GOVBR_REDIRECT_URI!,
-			SCOPES: process.env.GOVBR_SCOPES!,
-			CLIENT_ID: process.env.GOVBR_CLIENT_ID!,
-			SECRET: process.env.GOVBR_SECRET!,
-		};
-		const tokenResponse = await axios.post(`${config.URL_PROVIDER}/token`, {
-			code,
-			redirect_uri: config.REDIRECT_URI,
-			client_id: config.CLIENT_ID,
-			client_secret: config.SECRET,
-			grant_type: 'authorization_code',
-		});
-
-		const token = tokenResponse.data;
-		if (!token) {
-			return res.status(500).json({ message: 'Failed to obtain access token' });
-		}
-
-		console.log('Token:', token);
-		res.json({ access_token: token.access_token });
-	} catch (error) {
-		console.error('Erro ao trocar o código pelo token:', error);
-		res.status(500).json({ message: 'Erro ao trocar o código pelo token' });
-	}
-});
+app.use('/api', eventRouter);
+app.use('/api', userRouter);
+app.use('/api', courseRouter);
+app.use('/api', roleRouter);
+app.use('/api', studentRouter);
+app.use('/api', attendanceRouter);
+app.use('/api', reportsRouter);
+app.use('/api', eventStudentRouter);
 
 app.listen(4000, () => {
 	console.log(`App is listening on port 4000`);
