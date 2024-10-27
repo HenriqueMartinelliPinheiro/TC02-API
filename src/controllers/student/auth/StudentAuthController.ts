a4f49a0adcd5b5e79a25f75fb28ffc6af8623a40
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-10-27 09:31:16 -0300

    salvar token aluno no banco

--- src/controllers/student/auth/StudentAuthController.ts
+++ src/controllers/student/auth/StudentAuthController.ts
@@ -1,20 +1,20 @@
 import { Request, Response } from 'express';
 import { Logger } from '../../../loggers/Logger';
-import { StudentLoginService } from '../../../services/student/StudentLoginRedirect';
+import { StudentLoginRedirectService } from '../../../services/student/StudentLoginRedirect';
 import { AppError } from '../../../utils/errors/AppError';
 
 export class StudentAuthController {
-	private loginService: StudentLoginService;
+	private studentLoginRedirectService: StudentLoginRedirectService;
 	private logger: Logger;
 
-	constructor(loginService: StudentLoginService) {
-		this.loginService = loginService;
+	constructor(loginService: StudentLoginRedirectService) {
+		this.studentLoginRedirectService = loginService;
 		this.logger = new Logger('studentAuth', 'StudentAuthController');
 	}
 
 	login = (req: Request, res: Response) => {
 		try {
-			const redirectUrl = this.loginService.generateLoginUrl();
+			const redirectUrl = this.studentLoginRedirectService.generateLoginUrl();
 			res.redirect(redirectUrl);
 		} catch (error) {
 			if (error instanceof AppError) {
