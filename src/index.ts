1c8c95089c600c7806939be43810bdf2c48e37f1
HenriqueMartinelliPinheiro <henriquepinheiro18@gmail.com>
2024-10-27 18:11:32 -0300

    erro accesstoken

--- src/index.ts
+++ src/index.ts
@@ -20,28 +20,28 @@ const app: Express = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(
-// 	cors({
-// 		origin: ['http://200.135.55.14:5173', 'http://localhost:5173'],
-// 		allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie'],
-// 		credentials: true,
-// 	})
-// );
-
-const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',');
 app.use(
 	cors({
-		origin: (origin, callback) => {
-			if (!origin || allowedOrigins?.indexOf(origin) !== -1) {
-				callback(null, true);
-			} else {
-				callback(new Error('Not allowed by CORS'));
-			}
-		},
-		allowedHeaders: ['Content-Type', 'Authorization'],
+		origin: ['http://200.135.55.14:5173', 'http://localhost:5173'],
+		allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie'],
 		credentials: true,
 	})
 );
+
+// const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',');
+// app.use(
+// 	cors({
+// 		origin: (origin, callback) => {
+// 			if (!origin || allowedOrigins?.indexOf(origin) !== -1) {
+// 				callback(null, true);
+// 			} else {
+// 				callback(new Error('Not allowed by CORS'));
+// 			}
+// 		},
+// 		allowedHeaders: ['Content-Type', 'Authorization'],
+// 		credentials: true,
+// 	})
+// );
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
